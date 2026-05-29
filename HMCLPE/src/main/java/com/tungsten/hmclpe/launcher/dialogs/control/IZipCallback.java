1301427c51cc1192c63b2457336e4569231353ed
ibiszaderamin550-jpg <ibiszaderamin550@gmail.com>
2026-05-29 14:25:18 +0400

    Fix: Align fake IZipCallback methods with launcher expected parameters

--- HMCLPE/src/main/java/com/tungsten/hmclpe/launcher/dialogs/control/IZipCallback.java
+++ HMCLPE/src/main/java/com/tungsten/hmclpe/launcher/dialogs/control/IZipCallback.java
@@ -2,7 +2,7 @@ package com.tungsten.hmclpe.launcher.dialogs.control;
 
 public interface IZipCallback {
     void onStart();
-    void onProgress(int progress);
-    void onFinish();
+    void onProgress(int percentDone);
+    void onFinish(boolean success);
     void onError(Throwable t);
 }
